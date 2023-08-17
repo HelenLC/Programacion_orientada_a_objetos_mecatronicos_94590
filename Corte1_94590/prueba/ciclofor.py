bda444c4d3ad057dd9522ed2e1a084050ecd70e4
303 - Programacion <303@laboratorios.usbbog.edu.co>
2023-08-17 15:10:16 -0500

    actualizacion

--- Corte1_94590/prueba/ciclofor.py
+++
@@ -1,6 +0,0 @@
-#for= ciclo repetitivo, definido de un punto(inicio) a otro(final).
-#range= rango de una funcion
-#primo=divisible por si mismo y por la unidad ejem:2/2 si y 3/2 no
-
-for i in range(3,10,2):
-    print(i)
