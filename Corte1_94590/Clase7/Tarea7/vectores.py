7ea2f1a1286fc8e90c3fb5248c07b788b772beb1
303 - Programacion <303@laboratorios.usbbog.edu.co>
2023-09-05 13:49:25 -0500

    Tarea7

---
+++ Corte1_94590/Clase7/Tarea7/vectores.py
@@ -0,0 +1,8 @@
+def vectores():
+    x=int(input('Ingrese un valor: '))
+    y=int(input('Ingrese un valor: '))
+    m=math.sqrt(x**2+y**2)
+    print('El modulo del vectore es: ')
+
+if __name__=='__main__':
+    vectores()
