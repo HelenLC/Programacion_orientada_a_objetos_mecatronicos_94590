ddf17d98dad31f40d759ea0bc7dd8642c3815c38
Helen <hjlopezc2@academia.usbbog.edu.co>
2023-10-09 00:14:45 -0500

    actualizacion repositorio

---
+++ Corte2_94590/Clase1/Tarea1/punto1.py
@@ -0,0 +1,39 @@
+#realice un programa en donde permita  crear una lista de 10 números aleatorios entre 1 y 50 (todos numeros enteros)  despues cree tres funciones donde se reciba la lista como parametro para mayor, minimo y primos
+from random import randint as r
+def lista():
+    for i in range(10):
+        lista.append(r(1,50))
+        print(lista)
+    return lista
+
+def num_maximo(lista):
+    for numero in lista:
+        if numero>num_maximo:
+            num_maximo=numero(lista)
+        return num_maximo
+    
+def num_minimo(lista):
+    for numero in lista:
+        if numero<num_minimo:
+            num_minimo=numero(lista)
+        return num_minimo
+    
+def num_primo(numero):
+    if numero % 1 == numero:
+        return True
+    if numero % numero == 1:
+        return True
+    if numero % 2 == 0:
+        return False
+    
+def primo(lista):
+    primo = [numero for numero in lista if num_primo(numero)]
+    return primo
+
+print('El numero maximo es: ',[num_maximo])
+print('El numero minimo es: ',[num_minimo])
+print('El numero primo es: ',[primo])
+
+if __name__=='__main__':
+    lista=[]
+    lista()
---
+++ Corte2_94590/Clase2/lectura.py
@@ -0,0 +1,43 @@
+#rstrip=quitar caracteres desde el final
+#strip=quita caracteres desde el final
+#split= lista y se pone , o espacio segun lo que se quiere separar
+#\n= salto de linea
+def main_read():
+    f=open('matrizAsignacion.txt', 'rt')
+    documento=f.read()
+    f.close
+    print(documento)
+
+def main_read2():
+    f=open('matrizAsignacion.txt', 'rt')
+    documento=f.readline().rstrip('\n').split(',')
+
+    while documento != [""]: 
+        print(documento)
+        #input('Presione enter')
+        documento=f.readline().rstrip('\n').split(',')
+    f.close() 
+
+def suma(lista):
+    salida=''
+    for dato in lista:
+        result=int(dato[0])+int(dato[1])+int(dato[3])
+        salida+result+('\n')
+    return salida
+
+def main_read3():
+    f=open('matrizAsignacion.txt', 'rt')
+    documento=f.readlines()
+    f.close()
+    lista=[]
+    for dato in documento:
+        lista.append(dato.rstrip('\n').split(','))
+    print(lista)
+    a=suma(lista)
+    print(a)
+
+
+if __name__=='__main__':
+    #main_read()
+    #main_read2()
+    main_read3()
