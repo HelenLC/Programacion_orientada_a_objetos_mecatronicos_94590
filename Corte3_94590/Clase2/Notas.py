fd4087473ae2d8b60e1dae81cd877f7611f6e42f
Helen <hjlopezc2@academia.usbbog.edu.co>
2023-10-24 14:45:28 -0500

    clase33

--- Corte3_94590/Clase2/Notas.py
+++ Corte3_94590/Clase2/Notas.py
@@ -1,10 +1,10 @@
 class Estudiantes():
-    def __init__(self):     #Método Constructor
-        self.__nombre=None
-        self.__codigo=None
-        self.__carrera=None
-        self.__materia=None
-        self.__notas=None
+    def __init__(self, nombre:str, codigo:int, carrera:str, materia: str, notas: float):     #Método Constructor
+        self.__nombre=nombre
+        self.__codigo=codigo
+        self.__carrera=carrera
+        self.__materia=materia
+        self.__notas=notas
 
     #---------- Setters ------------
     def setNombre(self,nombre:str):
@@ -29,9 +29,44 @@ class Estudiantes():
         return self.__materia
     def getNotas(self):
         return self.__notas
+    
+
+#--------------Metodo---------------
+def __promedio(self):
+    n=self.getnotas()
+    return round(sum(n)/len(n),2)
+
+def getpromedio(self):
+    average=self.__promedio()
+    if average<3:
+        return f'El estudiante {self.getnombre()} '
 
 def main():
-    pass
+    while 1:
+        nota=[]
+        nombre=input('Nombre: ')
+        codigo=input('Codigo: ')
+        materia=input('Materia: ')
+        carrera=input('Carrera: ')
+        numero_notas=input('Cantidad de notas: ')
+        for i in range(int(numero_notas)):
+            nota.append(input(f'Nota {i+1}: '))
+
+        usuario=Estudiantes(nombre,codigo,carrera,materia,nota)
+        universidad.append(usuario)
+
+
+
+
+    usuario=Estudiantes
+    usuario.setNombre('Mariana')
+    usuario.setCodigo(115478536)
+    usuario.setMateria('Porgramación')
+    usuario.setCarrera('Ingenieria')
+    usuario.setNotas(2.5)
+
+    print(f'Nombre: {usuario.getNombre()}' f'Codigo: {usuario.getCodigo()}' f'Materia: {usuario.getMateria()} \n'\
+           f'Carrera: {usuario.getCarrera}'  f'Notas: {usuario.getNotas()}')
 
 if __name__=="__main__":
     main()
