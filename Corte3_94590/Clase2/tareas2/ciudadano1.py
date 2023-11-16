764f771c1b3a136f6e627a2783de63724ba9a49f
Helen <hjlopezc2@academia.usbbog.edu.co>
2023-11-16 09:47:16 -0500

    Clase2

--- Corte3_94590/Clase2/tareas2/ciudadano1.py
+++ Corte3_94590/Clase2/tareas2/ciudadano1.py
@@ -1,85 +1,60 @@
-class Ciudadano():
-    def __init__(self,nombre:str, edad:int, cc:int):
-        self._nombre=nombre
-        self._edad=edad
-        self._cc=cc
+class Ciudadano:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
 
-    def get_nombre(self):
-        return self._nombre
-    def get_edad(self):
-        return self._edad
-    def get_cc(self):
-        return self._cc 
-    
-    def set_nombre(self, nombre:str):
-        self._nombre=nombre 
+    def setNombre(self, nombre):
+        self.nombre = nombre
 
-    def set_edad(self, edad:int):
-        self._edad=edad
-    def set_cc(self, cc:int):
-         self._cc=cc
 
-    def _informacion(self):
-        return print(f'nombre{self._nombre}, edad{self._edad}, cc{self._cc}')
-    def legado(self):
-        return 'Gracias por su aporte y/o conocimiento'
-    
-class Lingüista(Ciudadano):
-    def __init__(self, nombre: str, edad: int, documento: int, especialidad:str,tiempo:int):
-        super().__init__(nombre, edad, documento)
-        self.__especialidad=especialidad
-        self.__tiempo=tiempo
-    def getEspecialidad(self):
-        return self.__especialidad
-    def getTiempo(self):
-        return self.__tiempo
-    def setNombre(self,especialidad:str):
-        self.__especialidad=especialidad
-    def setEdad(self,tiempo:int):
-         self.__tiempo=tiempo
-    def logro(self):
-         return 'Muchas gracias Sr/Sra.Lingüista'
-    
+    def setEdad(self, edad:int):
+        self.edad = edad
+
+
+    def getNombre(self):
+        return self.nombre
+
+    def getEdad(self):
+        return self.edad
+
+    def informacion(self):
+        print(f"Nombre: {self.nombre} es profesional en su profesion")
+
 class Profesor(Ciudadano):
-    def __init__(self, nombre: str, edad: int, documento: int, especialidad:str,tiempo:int):
-        super().__init__(nombre, edad, documento)
-        self.__especialidad=especialidad
-        self.__tiempo=tiempo
-    def getEspecialidad(self):
-        return self.__especialidad
-    def getTiempo(self):
-        return self.__tiempo
-    def setNombre(self,especialidad:str):
-        self.__especialidad=especialidad
-    def setEdad(self,tiempo:int):
-         self.__tiempo=tiempo
-    def logro(self):
-         return 'Muchas gracias Sr/Sra.Profesor'
-    
-class Paleontólogo(Ciudadano):
-    def __init__(self, nombre: str, edad: int, documento: int, especialidad:str,tiempo:int):
-        super().__init__(nombre, edad, documento)
-        self.__especialidad=especialidad
-        self.__tiempo=tiempo
-    def getEspecialidad(self):
-        return self.__especialidad
-    def getTiempo(self):
-        return self.__tiempo
-    def setNombre(self,especialidad:str):
-        self.__especialidad=especialidad
-    def setEdad(self,tiempo:int):
-         self.__tiempo=tiempo
-    def logro(self):
-         return 'Muchas gracias Sr/Sra.	Paleontólogo'
-        
-def main():
-    profecional=[]
-    profecional1=Lingüista('Maria',42,1150487922,'Sintaxis',15)
-    profecional2=Profesor('Eduardo',35,101525483,'profesor',10)
-    profecional3=Paleontólogo('Asucena',60,1022287433,'Anatomía comparada',30)
-    profecional.append(profecional1)
-    profecional.append(profecional2)
-    profecional.append(profecional3)
+    def __init__(self, nombre, edad, asignatura):
+        super().__init__(nombre, edad)
+        self.asignatura = asignatura
+    def dame_info(self):
+        return f"{self.nombre} es un profesor de {self.asignatura}"
+class Medico(Ciudadano):
+    def __init__(self, nombre, edad, especialidad):
+        super().__init__(nombre, edad)
+        self.especialidad = especialidad
 
-if __name__=='__main__':
+    def atender_paciente(self):
+        return f"{self.nombre} está atendiendo a un paciente"
+
+class Ingeniero(Ciudadano):
+    def __init__(self, nombre, edad, especialidad):
+        super().__init__(nombre, edad)
+        self.especialidad = especialidad
+    def investigar(self):
+        return f"{self.nombre} está investigando en {self.especialidad}"
+
+    def investigar(self, tema):
+        return f"{self.nombre} está investigando {tema} en {self.especialidad}"
+    
+            
+def main():
+    pro = Profesor("Juan", 35, "Matemáticas")
+    print(pro.dame_info())
+    
+    med = Medico("María", 40, "Cardiología")
+    print(med.atender_paciente())
+    
+    ing = Ingeniero("Pedro", 30, "Informática")
+    print(ing.investigar("Virus"))
+
+
+if __name__ == "__main__":
     main()
